dfa5cfd867f97c740f03f54fd258fe11c430bb06
Maryam Zahid <maryamzahid960@gmail.com>
2025-06-29 14:39:39 +0500

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Start Apache in the background
-apachectl -D FOREGROUND &
+# Start Apache in background
+service apache2 start
 
-# Wait a few seconds to ensure Apache is up
+# Optional delay to wait for server to be up
 sleep 5
 
 # Run Selenium tests
 python3 -m unittest -v tests/test_taskmanager.py
+
+# Keep container running
+tail -f /dev/null
